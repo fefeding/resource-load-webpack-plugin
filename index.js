e4d3a78d1f92b0aa90151792e91601fb72ea2d41
fefeding <fefeding@jintengtech.com>
2022-05-18 15:50:39 +0800

    :art: 增加缓存清理

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ class JTResourceLoad {
                             // 当缓存中的url是当前url才表示命中，否则为不同版本，不能采用
                             "if(text && text.indexOf('//' + url) === 0) return text;",
                         "}",
-                        "else window.localStorage.setItem(url, '//' + url + '\n' + data);",
+                        "else window.localStorage.setItem(url, '//' + url + '\\n' + data);",
                         "} catch(e) {console.error(e);",
                             "if(e.name === 'QuotaExceededError') {",
                                 "window.localStorage.clear && window.localStorage.clear();",
